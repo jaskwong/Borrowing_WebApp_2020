e63cb14e1dea9c2477f5173c90ddac9ccb367a67
Tonyxu74 <tonyxu74@hotmail.com>
2020-08-25 00:28:36 -0600

    have front/backend communicating

---
+++ borrow_webpage/src/App.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {groupid: "", apiResponse: ""};
+  }
+
+  componentWillMount() {
+    //startup page information
+  }
+
+  changeViewGroup = event => {
+    this.setState({groupid: event.target.value});
+  }
+
+  submitViewGroup = event => {
+    event.preventDefault();
+
+    const groupid = this.state.groupid
+
+    axios.get("http://localhost:9000/testcreategroup/viewgroup", { params:{groupid: groupid} })
+        .then(res => this.setState({apiResponse:JSON.stringify(res.data)}))
+  }
+
+
+  render () {return (
+    <div className="App">
+    <header className="App-header">
+      <form onSubmit={this.submitViewGroup}>
+        <label for={"GroupID"}>Group ID:</label>
+        <input type={"text"} id={"GroupID"} name={"GroupID"} onChange={this.changeViewGroup}/>
+        <input type={"submit"} value={"Submit"}/>
+      </form>
+      <p className={"App-intro"}>{this.state.apiResponse}</p>
+    </header>
+  </div>)};
+}
+
--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testcreategroupRouter = require('./routes/testcreategroup')
+const bodyParser= require('body-parser')
 
 
 var app = express();
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
